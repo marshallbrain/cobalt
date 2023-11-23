8644936addaeda66eb0009e515ac9b30dc698b32
Marshall Brain <marshalldbrain@gmail.com>
2023-11-23 10:18:03 -0500

    Implement infinite scroll for gallery

--- app/routes/gallery.tsx
+++ app/routes/gallery.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {forwardRef, useCallback, useEffect, useState} from 'react';
 import SearchBar from "~/components/SearchBar";
 import {useSearchParams, useSubmit} from "@remix-run/react";
 import type { LoaderFunctionArgs} from "@remix-run/node";
@@ -6,6 +6,8 @@ import {json} from "@remix-run/node";
 import {db} from "~/db/database.server";
 import type {SelectQueryBuilder} from "kysely";
 import type {DB} from "~/db/types";
+import { VirtuosoGrid } from 'react-virtuoso';
+import {Box, Paper, Skeleton, Stack, styled} from "@mui/material";
 
 const regex = /"(.+)"|(\w+)/g
 
@@ -25,7 +27,7 @@ export async function loader({
 
     query = queryOrder(query, params.get("sort") ?? "name", params.has("order", "true"))
 
-    console.log(query.compile())
+    // console.log(query.compile())
 
     return json({})
 }
@@ -45,14 +47,58 @@ function queryOrder(query: Query, order: string, dir: boolean) {
     }
 }
 
+const ItemWrapper = styled("div")(theme => ({
+    flexBasis: 100/6 + "%",
+    maxWidth: 100/6 + "%",
+    display: "flex",
+    flexDirection: "column",
+}))
+
+const GalleryWrapper = styled("div")(({theme}) => ({
+    display: "flex",
+    flexWrap: "wrap"
+}))
+
 export default function Gallery() {
     const [search] = useSearchParams()
     const setSearch = useSubmit()
+    const [users, setUsers] = useState<number[]>(() => [1, 2, 3, 4, 5, 6])
+
+    const loadMore = useCallback(() => setUsers(users => [...users, ...Array(16).keys()]), [setUsers])
+
+    useEffect(() => {
+        loadMore()
+    }, [loadMore])
 
     return (
-        <>
+        <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="stretch"
+            spacing={0}
+            sx={{height: "100%"}}
+        >
             <SearchBar search={Object.fromEntries(search.entries())} onSearch={setSearch}/>
-        </>
+            <VirtuosoGrid
+                data={users}
+                endReached={loadMore}
+                overscan={8}
+                components={{
+                    Item: ItemWrapper,
+                    List: GalleryWrapper as any,
+                }}
+                itemContent={(index) => (<Paper sx={{margin: 0.5}}>
+                    <Skeleton variant="rectangular" width={"100%"} height={"auto"} sx={{
+                        aspectRatio: 1,
+                        borderTopLeftRadius: "inherit",
+                        borderTopRightRadius: "inherit",
+                    }}/>
+                    <Box sx={{padding: 0.5}}>
+                        <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    </Box>
+                </Paper>)}
+            />
+        </Stack>
     )
 }
 
