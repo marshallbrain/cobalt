1d0861b0798633c4a77dffceb1b3421ed7f578bc
Marshall Brain <marshalldbrain@gmail.com>
2023-11-28 14:04:17 -0500

    Implement different scaling options

--- app/routes/view.$id.tsx
+++ app/routes/view.$id.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import type {LoaderFunctionArgs, MetaFunction} from "@remix-run/node";
 import {json} from "@remix-run/node";
-import {AppBar, Box, Chip, styled, Toolbar, Typography} from '@mui/material';
+import {AppBar, Box, Chip, styled, Theme, ToggleButton, ToggleButtonGroup, Toolbar, Typography} from '@mui/material';
 import {db} from "~/db/database.server";
 import {useLoaderData} from "@remix-run/react";
+import AspectRatioRounded from "~/components/icons/AspectRatioRounded";
+import FitScreenRounded from "~/components/icons/FitScreenRounded";
+import PhotoSizeLargeRounded from '~/components/icons/PhotoSizeLargeRounded';
 
 export const meta: MetaFunction = () => {return [{ title: "ViewId" }]}
 
-const Img = styled("img")(({theme}) => ({
-    maxHeight: "calc(100vh - 64px)",
+const Img = styled("img", {
+    shouldForwardProp: (prop) => prop !== 'dir' && prop !== 'size'
+})<{ zoom: string, aspect: number }>(({theme, zoom, aspect}) => ({
 }))
+
+function buildImageStyle ({theme, zoom, direct}: {theme: Theme, zoom: string, direct: string}) {
+    return {
+        maxHeight: "calc(100vh - 64px)",
+        maxWidth: "100%",
+        ...(zoom === "fill" && {flexGrow: 1}),
+        objectFit: "contain"
+    }
+}
 
 export async function loader({
     request, params
@@ -27,6 +40,11 @@ export async function loader({
 
 export default function ViewId() {
     const photo = useLoaderData<typeof loader>()
+    const [photoSize, setPhotoSize] = useState<string>("fit")
+
+    const handleAlignment = (event: React.MouseEvent<HTMLElement>, value: string | null,) => {
+        if (value) setPhotoSize(value)
+    }
 
     return (
         <Box sx={{display: "flex", flexDirection: "column", minHeight: "100vh", maxHeight: "100vh"}}>
@@ -36,22 +54,59 @@ export default function ViewId() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, pl: 2 }}>
                         {photo.photo_name}
                     </Typography>
+                    <ToggleButtonGroup
+                        exclusive
+                        value={photoSize}
+                        onChange={handleAlignment}
+                    >
+                        <ToggleButton value="zoom" disabled>
+                            <PhotoSizeLargeRounded/>
+                        </ToggleButton>
+                        <ToggleButton value="fill">
+                            <FitScreenRounded/>
+                        </ToggleButton>
+                        <ToggleButton value="fit">
+                            <AspectRatioRounded/>
+                        </ToggleButton>
+                    </ToggleButtonGroup>
                 </Toolbar>
             </AppBar>
-            <Box sx={{flexGrow: "1", display: "flex", justifyContent: "center"}}>
-                <Img
-                    alt={photo.photo_name}
-                    src={"/photo/".concat(
-                        photo.photo_id.toString(),
-                        photo.photo_type,
-                        "?",
-                        new URLSearchParams({
-                            w: (photo.photo_width).toString(),
-                            h: (photo.photo_height).toString()
-                        }).toString()
-                    )}
-                    loading="lazy"
-                />
+            <Box sx={{
+                flexGrow: "1",
+                overflow: "auto",
+                height: "100%",
+                position: "relative"
+            }}>
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    position: "absolute",
+                    minWidth: "100%",
+                    minHeight: "100%",
+                    "& img": {
+                        maxHeight: "calc(100vh - 64px)",
+                        maxWidth: "100%",
+                        ...(photoSize !== "fit" && {flexGrow: 1}),
+                        objectFit: "contain"
+                    }
+                }}>
+                    <Img
+                        zoom={photoSize}
+                        aspect={photo.photo_width/photo.photo_height}
+                        alt={photo.photo_name}
+                        src={"/photo/".concat(
+                            photo.photo_id.toString(),
+                            photo.photo_type,
+                            "?",
+                            new URLSearchParams({
+                                w: photo.photo_width.toString(),
+                                h: photo.photo_height.toString()
+                            }).toString()
+                        )}
+                        loading="lazy"
+                    />
+                </Box>
             </Box>
         </Box>
     )
